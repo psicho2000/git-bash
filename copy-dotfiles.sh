934723e77c49ce7ab91dcd8dae2b573d220b4dc6
Markus Meier <markus.meier@gbtec.de>
2018-11-12 12:52:20 +0100

    rename script

---
+++ copy-dotfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+dotfiles="$HOME/git-bash"
+
+copy() {
+  from="$1"
+  to="$2"
+  echo "Linking '$from' to '$to'"
+  rm -f "$to"
+  ln -s "$from" "$to"
+}
+
+if [[ -d "$dotfiles" ]]; then
+  echo "Copying dotfiles from $dotfiles"
+else
+  echo "$dotfiles does not exist"
+  exit 1
+fi
+
+cd $dotfiles
+
+for location in $(find . -maxdepth 1 -type f -name '.*' -printf '%f\n'); do
+  if [[ $location != "./.gitignore" ]]; then
+    copy "$dotfiles/$location" "$HOME/$location"
+  fi
+done
