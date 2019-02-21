17207b6744f1fb7228fc4bc753f071ffea28748e
Markus Meier <markus.meier@gbtec.de>
2019-02-21 08:56:30 +0100

    git aliases

---
+++ utils/install-git-aliases.sh
@@ -0,0 +1,13 @@
+# Add & commit
+git config --global alias.ac '!git add . && git commit -am'
+
+# Undo Commit
+git config --global alias.undo-commit 'reset --soft HEAD~1'
+
+# Check out the last branch you were on
+git config --global alias.col 'checkout @{-1}'
+
+git config --global alias.new 'checkout -b'
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.co checkout
+git config --global alias.last 'log -1 HEAD'
