d94801047dcde005efa3d5b3aecd836dce92e911
psicho2000 <markus.d.meier@gmail.com>
2019-03-01 00:31:45 +0100

    Update setup.sh

--- bash-utils/setup.sh
+++ bash-utils/setup.sh
@@ -5,7 +5,6 @@ function __cfg {
   /mingw64/bin/git --git-dir=$HOME/.git-bash --work-tree=$HOME $@
 }
 
-cd $HOME
 git clone --bare https://github.com/psicho2000/git-bash $HOME/.git-bash
 __cfg config --local core.autocrlf input
 __cfg config --local status.showUntrackedFiles no
