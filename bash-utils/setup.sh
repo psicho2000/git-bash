576d766ec1011c8ff7ec6f62706482c4f22b4828
psicho2000 <markus.d.meier@gmail.com>
2019-03-01 00:21:44 +0100

    Update setup.sh

--- bash-utils/setup.sh
+++ bash-utils/setup.sh
@@ -2,14 +2,15 @@
 # Call via `curl -Lks https://bit.ly/setup-cfg | /bin/bash`
 
 function __cfg {
-  /mingw64/bin/git --git-dir=$HOME/.git-bash --work-tree=$HOME $@
+  /mingw64/bin/git --git-dir=~/.git-bash --work-tree=~ $@
 }
 
 cd ~
-echo ".git-bash" >> .gitignore
-git clone --bare https://github.com/psicho2000/git-bash $HOME/.git-bash
+echo ".git-bash" >> ~/.gitignore
+git clone --bare https://github.com/psicho2000/git-bash ~/.git-bash
 __cfg config --local core.autocrlf input
 __cfg config --local status.showUntrackedFiles no
 __cfg checkout
+__cfg push -u origin HEAD
 ~/bash-utils/configure-git.sh
 cp ~/bash-utils/.settings.example ~/bash-utils/.settings
