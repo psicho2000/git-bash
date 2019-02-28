5b65c0df2d323743b2ca00ec69755093c3618eaf
psicho2000 <markus.d.meier@gmail.com>
2019-02-28 23:36:22 +0100

    Create setup.sh

---
+++ bash-utils/setup.sh
@@ -0,0 +1,9 @@
+cd ~
+alias cfg='/mingw64/bin/git --git-dir=$HOME/.git-bash --work-tree=$HOME'
+echo ".git-bash" >> .gitignore
+git clone --bare https://github.com/psicho2000/git-bash $HOME/.git-bash
+cfg config --local core.autocrlf input
+cfg config --local status.showUntrackedFiles no
+cfg checkout
+bash-utils/configure-git.sh
+cp ~/bash-utils/.settings.example ~/bash-utils/.settings
